afb730c5feeeadfc088edcbffac74b991b3d15c6
s-zaekov <102063707+s-zaekov@users.noreply.github.com>
2022-03-21 21:55:46 +0200

    Delete 06_november directory

--- 06_november/number_100_200.py
+++
@@ -1,8 +0,0 @@
-number = int(input())
-
-if number < 100:
-    print("Less than 100")
-elif number > 200:
-    print('Greater than 200')
-elif 100 <= number <= 200:
-    print('Between 100 and 200')
--- 06_november/password_guess.py
+++
@@ -1,6 +0,0 @@
-password = str(input())
-
-if password == 's3cr3t!P@ssw0rd':
-    print('Welcome')
-else:
-    print('Wrong password!')
